dca5ca18e05d9932267fa8f449d68dcc73fe8bb6
nombulelo123 <nombulelosnothando92@gmail.com>
2025-08-12 15:25:15 +0200

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const code = require('./pair'); // moved to same folder for Vercel
+
+app.use('/code', code);
+
+app.use('/pair', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/pair.html'));
+});
+
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/main.html'));
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+module.exports = app;
+        
